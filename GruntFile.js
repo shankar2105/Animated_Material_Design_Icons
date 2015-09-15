ec085d5371abc3a97808bd640632dc3995372b49
shankar2105 <shankar2105@gmail.com>
2015-09-15 23:24:54 +0530

    Initial Commit

---
+++ GruntFile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'css/md_svg_animation.css' : 'sass/core.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
